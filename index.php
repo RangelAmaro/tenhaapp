a7780d12fdf016663f10c78a8e159c768438a61c
Rangel Amaro de Souza <rangelamaro90@gmail.com>
2023-10-13 11:10:02 -0300

    primeiro comitt

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tenha App</title>
+    <meta name="description" content="Transforme seu site em um aplicativo nativo">
+</head>
+<body>
+    <h1>Tenha App</h1>
+    <p>estamos em desenvolvimento</p>
+    <p>contato@tenha.app</p>
+    <p>19981284821</p>
+</body>
+</html>
