4086523f6dc8455be42cd9101f896f229fd89d8b
Rangel Amaro de Souza <rangelamaro90@gmail.com>
2023-10-13 22:58:53 -0300

    HOME resolvendo problema css

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tenha App</title>
     <meta name="description" content="Transforme seu site em um aplicativo nativo">
-    <link rel="stylesheet" href="/assets/css/folha.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
     <link rel="stylesheet" href="/assets/font/style.css">
 </head>
 
