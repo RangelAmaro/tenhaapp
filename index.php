620ae5a739b2cb927f76321ea87161c4879fa9cb
Rangel Amaro de Souza <rangelamaro90@gmail.com>
2023-10-13 22:54:37 -0300

    HOME version 1

--- index.php
+++ index.php
@@ -1,16 +1,57 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tenha App</title>
     <meta name="description" content="Transforme seu site em um aplicativo nativo">
     <link rel="stylesheet" href="/assets/css/folha.css">
+    <link rel="stylesheet" href="/assets/font/style.css">
 </head>
+
 <body>
-    <h1>Tenha App</h1>
-    <p>estamos em desenvolvimento</p>
-    <p>contato@tenha.app</p>
-    <p>19981284821</p>
+    <div class="inicio-home">
+        <section class="branding-app">
+            <div class="chamado-home">
+                <p>SUA EMPRESA NO <strong>CELULAR DO CLIENTE</strong></p>
+            </div>
+            <div class="play-store-view">
+                <div class="info-app">
+                    <img src="assets/img/logo-roxa.png" alt="Logo Tenha App">
+                    <div class="info-info-app">
+                        <h1>Tenha App</h1>
+                        <h2>Tenha App</h2>
+                        <h3>Compras no app</h3>
+                    </div>
+                </div>
+                <div class="dados-app-play-store">
+                    <span>
+                        <p class="estrelas-app" >4,9 <i class="icon-star-full"></i></p>
+                        <p class="descrição-dados-app" >116 avaliações</p>
+                    </span>
+                    <span class="borda-dupla-dir-esq" >
+                        <i class="icon-box-add"></i>
+                        <p class="descrição-dados-app" >9,0 MB</p>
+                    </span>
+                    <span class="livre" >
+                        <span class="l-livre" >
+                            L
+                        </span>
+                    </span>
+                </div>
+                <a href="#" class="acao-play-store" >
+                Criar meu aplicativo
+                </a>
+            </div>
+        </section>
+        <span class="role-pra-baixo-home">
+                <p>Role para baixo</p>
+                <img class="w-45px" src="assets/img/seta-baixo.gif" alt="Role para baixo">
+        </span>
+    </div>
+<br><br><br><br><br>
+    <h1>ola mundo</h1>
 </body>
+
 </html>
