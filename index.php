4d7d190914c708d0658ad1022fa5d7de88dd4048
Rangel Amaro de Souza <rangelamaro90@gmail.com>
2023-10-21 21:31:58 -0300

    Versão pagina 1.02

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 <html lang="pt-br">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <!--<script async src="https://www.googletagmanager.com/gtag/js?id=G-B3XRMPJ0ZK"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-B3XRMPJ0ZK');
+    </script>-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tenha App</title>
@@ -12,29 +24,33 @@
 </head>
 
 <body>
+    <header>
+        <img src="/assets/img/logo-1(branca).png" alt="logo tenha app">
+    </header>
     <main class="main-home">
-        <header>
-            <img src="/assets/img/logo-1(branca).png" alt="logo tenha app">
-        </header>
         <section class="orcamento">
-            <h1>Converta seu site em app</h1>
-            <form action="processa_formulario.php" method="post">
-                <label for="nome">Nome:</label>
-                <input type="text" id="nome" name="nome" required><br>
+            <h1>Converta seu site em <span class="c-roxo">app</span></h1>
+            <form class="form-orcamento" action="processa_formulario.php" method="post">
+                <label for="nome">Nome do negócio</label>
+                <input type="text" id="nome" name="nome" placeholder="Nome da empresa" required>
 
-                <label for="email">E-mail:</label>
-                <input type="email" id="email" name="email" required><br>
+                <label for="email">Contato</label>
+                <input type="text" id="email" name="email" placeholder="Numero ou e-mail" required>
 
-                <label for="link">Link:</label>
-                <input type="text" id="link" name="link" required><br>
+                <label for="link">Seu site</label>
+                <input type="text" id="link" name="link" placeholder="Link do site" required>
 
-                <input type="submit" value="Enviar">
+                <input class="botao-orcamento" type="submit" value="Enviar">
             </form>
         </section>
-        <div class="explica-home">
-            <h2>Oque acontece <strong>depois?</strong></h2>
-            <p>Vamos entrar em contato e te enviar o nosso orçamento, e esclarecer dúvidas</p>
-        </div>
+        <section class="txt-foto">
+            <img src="/assets/img/User-flow-amico.png" alt="Tenha app ilustração">
+            <div class="explica-home">
+                <h2>Oque acontece <strong>depois?</strong></h2>
+                <p>Vamos entrar em contato e te enviar o nosso orçamento, e esclarecer dúvidas</p>
+            </div>
+        </section>
+
     </main>
 </body>
 
